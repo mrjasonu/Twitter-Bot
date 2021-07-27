2c3c7323d102ce4aba28f5ce7462eb6a49ec74a3
jumahi <mrjasonu@gmail.com>
2021-07-27 16:36:17 +0100

    Can print tweetes with a specific hashtag

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ auth = tweepy.OAuthHandler(data.consumer_key, data.consumer_secret_key)
 auth.set_access_token(data.access_token, data.access_token_secret)
 
 api = tweepy.API(auth)
-api.update_status('@JasonTheCoder Congratulations!!!')
+# api.update_status('@JasonTheCoder Congratulations!!!')
+
+tweets = api.mentions_timeline()
+for tweet in tweets:
+    if '#ultimatebot' in tweet.text.lower():
+        print(f'{tweet.id} - {tweet.text}')
